d8a2601014923196cba5ee5605e4fa75059d6649
jriflying12 <47150447+jriflying12@users.noreply.github.com>
2021-11-09 22:14:41 +0400

    Add files via upload

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
   style: "mapbox://styles/julienrichard/cjrvpus351myv1fp3vgedmvzf",
   accessToken:
-    "pk.eyJ1IjoianVsaWVucmljaGFyZCIsImEiOiJjaXBpYWVjcGcwMDNmdmJrd3NmOW5yc2IxIn0.B6a6wxJn_XYYLP8j23nyrg",
+    "pk.eyJ1IjoianVsaWVucmljaGFyZCIsImEiOiJja3ZzZXdieDM0bm51MnZxNXNwczN3OW05In0.J724SPwmyb2jRD7ehdmp8g",
   CSV: "./liste__Reference_Revue.csv",
   center: [48.88,2.32],
   zoom: 12,
@@ -9,7 +9,7 @@ const config = {
   description:
     "Creches et maternelles a Paris",
   sideBarInfo: ["nom_etablissement","statut_public_prive","section", "adresse_1", "code_postal"],
-  popupInfo: ["popup"],
+  popupInfo: ["web"],
   filters: [
     {
       type: "checkbox",
