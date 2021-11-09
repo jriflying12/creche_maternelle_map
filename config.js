f8688ffc9d11a4a35982a0f9a65ea1805890428c
jriflying12 <47150447+jriflying12@users.noreply.github.com>
2021-11-09 22:06:12 +0400

    Add files via upload

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
   style: "mapbox://styles/julienrichard/cjrvpus351myv1fp3vgedmvzf",
   accessToken:
     "pk.eyJ1IjoianVsaWVucmljaGFyZCIsImEiOiJjaXBpYWVjcGcwMDNmdmJrd3NmOW5yc2IxIn0.B6a6wxJn_XYYLP8j23nyrg",
-  CSV: "../liste__Reference_Revue.csv",
+  CSV: "./liste__Reference_Revue.csv",
   center: [48.88,2.32],
   zoom: 12,
   title: "Creches et maternelles a Paris",
